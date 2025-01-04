d088eed4448380d6f37d19e16e1a1a334b82a5b8
dusk-beep <dusk10037@gmail.com>
2025-01-04 22:00:21 +0530

    added checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
         {todos.map((elem, index) => {
           return (
             <li key={index}>
+              <input type='checkbox' id={`todo-${index}`} />
               {elem}
               <button
                 onClick={() => {
