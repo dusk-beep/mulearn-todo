f108447d01cba3b941e2188b5f97017664755afe
dusk-beep <dusk10037@gmail.com>
2025-01-04 20:46:49 +0530

    react todo

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  const [newTodo, setNewTodo] = React.useState('');
+
+  function handleInputChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleTOdos(event) {
+    event.preventDefault();
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  }
+
+  function handleRemoveTodo(index) {
+    setTodos(todos.filter((_, ind) => ind != index));
+  }
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleTOdos}>
+        <input
+          type='text'
+          value={newTodo}
+          onChange={handleInputChange}
+          placeholder='Add a new Todo'
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
+
+      <ul>
+        {todos.map((elem, index) => {
+          return (
+            <li key={index}>
+              {elem}
+              <button
+                onClick={() => {
+                  handleRemoveTodo(index);
+                }}
+              >
+                Remove Todo
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+export default App;
